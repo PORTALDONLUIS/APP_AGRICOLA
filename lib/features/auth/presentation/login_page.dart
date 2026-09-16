fccc7dccd45a7bd4947b8892f860f0b0d78e1b1e
PORTALDONLUIS <soporteti@agricoladonluis.com>
2026-09-16 17:54:12 -0500

    YERSON: FIX - Funcionalidad Actualizar APP-prueba-2

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -141,7 +141,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
 
                         const SizedBox(height: 18),
                         Text(
-                          'Bienvenido - 2026',
+                          'Bienvenido',
                           textAlign: TextAlign.center,
                           style: textTheme.headlineSmall?.copyWith(
                             color: Colors.white,
